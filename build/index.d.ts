abb4a09112ce3d46fc6d6140692878e72ee69a3a
Sahil <aggarwalsahil2004@gmail.com>
2022-08-24 16:17:17 +0530

    pitch rate volume added

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,12 +1,17 @@
 import { CSSProperties } from 'react';
 declare type Id = string | number | null;
 declare type Button = JSX.Element | string;
+declare type Number = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+declare type Volume = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 declare type Props = {
-    id?: Id;
+    id: Id;
     text: string;
-    style?: CSSProperties;
-    startBtn?: Button;
-    stopBtn?: Button;
+    style: CSSProperties;
+    startBtn: Button;
+    stopBtn: Button;
+    pitch: Number;
+    rate: Number;
+    volume: Volume;
 };
-declare function Speech({ id, text, style, startBtn, stopBtn }: Props): JSX.Element;
+declare function Speech({ id, text, style, startBtn, stopBtn, pitch, rate, volume }: Props): JSX.Element;
 export default Speech;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React, { CSSProperties, useState } from 'react'
 
+/** @defaultValue null */
 type Id = string | number | null
 
 type Button = JSX.Element | string
 
-type Props = { id?: Id, text: string, style?: CSSProperties, startBtn?: Button, stopBtn?: Button }
+/** @defaultValue 5 */
+type Number = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
 
-function Speech({ id = null, text, style = {}, startBtn = <button>Start Speech</button>, stopBtn = <button>Stop Speech</button > }: Props): JSX.Element {
+/** @defaultValue 10 */
+type Volume = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+
+type Props = { id: Id, text: string, style: CSSProperties, startBtn: Button, stopBtn: Button, pitch: Number, rate: Number, volume: Volume }
+
+function Speech({ id = null, text, style = {}, startBtn = <button>Start Speech</button>, stopBtn = <button>Stop Speech</button >, pitch = 5, rate = 5, volume = 10 }: Props): JSX.Element {
     const [speechIcon, setSpeechIcon] = useState<Button>(startBtn)
     const [speechId, setSpeechId] = useState<Id>(null)
 
@@ -16,6 +23,9 @@ function Speech({ id = null, text, style = {}, startBtn = <button>Start Speech</
         // below is the method to speak:
         // speechSynthesis.speak(new SpeechSynthesisUtterance(text to be spoken))
         const utterance = new SpeechSynthesisUtterance(text?.replace(/\s/g, ' '))
+        utterance.pitch = pitch / 5
+        utterance.rate = rate / 5
+        utterance.volume = volume / 10
         speechSynthesis.speak(utterance)
         utterance.onend = () => {
             setSpeechId(null)
